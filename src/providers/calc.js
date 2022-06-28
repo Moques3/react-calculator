64be1112bb796a36e6e6c5da76080df23197c6b6
bea <anna.eu.bia@gmail.com>
2022-06-28 10:57:23 -0300

    funcao da soma

--- src/providers/calc.js
+++ src/providers/calc.js
@@ -3,18 +3,41 @@ import { createContext, useState } from "react";
 export const CalcContext = createContext([]);
 
 export function CalcProvider({ children }) {
-    const [value, setValue] = useState(0);
+    const [value, setValue] = useState('0');
 
     function changeValue(newValue) {
-        setValue(newValue);
+        if (value.includes('/') || value.includes('+') || value.includes('-') || value.includes('*')) {
+            setValue(value + newValue);
+        }
+        else{
+            if (value != '0' && value.length >= 1) {
+                setValue(value + newValue);
+            }
+            else {
+                setValue(newValue +``);
+            }
+        }
     }
 
     function clean() {
-        setValue(0);
+        setValue('0');
+    }
+
+    function calc() {
+        let parts = value.split(`+`);
+        setValue(parseInt(parts[0]) + parseInt(parts[1]) + '');
     }
 
     function operation(operator) {
-        alert(operator);
+        if (operator == 'AC'){
+            clean();
+        }
+        else if (operator == `=`) {
+            calc();
+        }
+        else {
+            setValue(value+operator);
+        }
     }
 
     return (
